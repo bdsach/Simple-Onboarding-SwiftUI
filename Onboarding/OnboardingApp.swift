7d2cfa2ea05fc217e2b4eda34defa3f631c658b9
Bandit Silachai <mbandit001@yahoo.co.th>
2023-12-13 11:48:59 +0700

    Initial Commit

---
+++ Onboarding/OnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingApp.swift
+//  Onboarding
+//
+//  Created by Bandit Silachai on 13/12/23.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
