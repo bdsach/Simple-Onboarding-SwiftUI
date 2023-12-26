78fa580582611cd4afd1bc15bcb0f4ab99e55f4b
Bandit Silachai <mbandit001@yahoo.co.th>
2023-12-26 12:48:37 +0700

    🎉 Initial commit

--- Onboarding/ContentView.swift
+++ Onboarding/ContentView.swift
@@ -5,17 +5,49 @@
 //  Created by Bandit Silachai on 13/12/23.
 //
 
+
 import SwiftUI
 
+struct Screen: Identifiable {
+    let id = UUID()
+    let icon: String
+    let color: Color
+}
+
 struct ContentView: View {
+    
+    let data: [Screen] = [
+        Screen(icon: "figure.skiing.downhill", color: .yellow),
+        Screen(icon: "figure.archery", color: .green),
+        Screen(icon: "figure.baseball", color: .pink),
+        Screen(icon: "figure.cooldown", color: .blue),
+    ]
+    
+    var body: some View {
+        TabView {
+            ForEach(data, id: \.id) { item in
+                DetailView(color: item.color, icon: item.icon)
+            }
+        }
+        .ignoresSafeArea()
+        .tabViewStyle(.page(indexDisplayMode: .always))
+        .indexViewStyle(.page(backgroundDisplayMode: .always))
+    }
+}
+
+struct DetailView: View {
+    
+    var color: Color
+    var icon: String
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: icon)
+                .font(.system(size: 80))
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(color.opacity(0.6))
+        
     }
 }
 
